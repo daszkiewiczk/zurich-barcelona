e5211514a809f3d7d2fcc23454c3f2bb1645ec3f
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-07-15 14:24:55 +0200

    v0

---
+++ tf/outputs.tf
@@ -0,0 +1,9 @@
+# output "webapp_eip_a" {
+#   description = "Ip of webbapp instance a."
+#   value       = module.webapp.eip_a
+# }
+
+# output "webapp_eip_b" {
+#   description = "Ip of webbapp instance b."
+#   value       = module.webapp.eip_b
+# }
