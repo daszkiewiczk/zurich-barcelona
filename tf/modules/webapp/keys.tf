c74555f051ae90430f201da046abbadf542ca36b
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-07-15 15:29:01 +0200

    keep tf manifests and app code in same dir

--- tf/modules/webapp/keys.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_key_pair" "zurich_webapp_key_a" {
-  key_name   = "zurich_webapp_key_a"
-  public_key = file("${path.module}/keys/webapp_key_a.pub")
-}
-
-resource "aws_key_pair" "zurich_webapp_key_b" {
-  key_name   = "zurich_webapp_key_b"
-  public_key = file("${path.module}/keys/webapp_key_b.pub")
-}
--- tf/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "webapp_eip_a" {
-#   description = "Ip of webbapp instance a."
-#   value       = module.webapp.eip_a
-# }
-
-# output "webapp_eip_b" {
-#   description = "Ip of webbapp instance b."
-#   value       = module.webapp.eip_b
-# }
